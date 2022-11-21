a6cf692d2a982d4f0ee9b67a51ae66f71c62f9f2
E87EHUU <105707796+E87EHUU@users.noreply.github.com>
2022-11-21 23:30:11 +0300

    Урок 8. переопределен метод onCreateOptionsMenu в NoteDetail. // Добавлена кнопка "⋮" в детализации заметки

--- app/src/main/java/com/example/mynotesapplication/NoteDetail.java
+++ app/src/main/java/com/example/mynotesapplication/NoteDetail.java
@@ -12,6 +12,9 @@ import androidx.fragment.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -31,8 +34,21 @@ public class NoteDetail extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (savedInstanceState != null)
+        setHasOptionsMenu(true);
+        /* if (savedInstanceState != null)
             requireActivity().getSupportFragmentManager().popBackStack();
+
+         */
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        MenuItem menuItemExit = menu.findItem(R.id.action_exit);
+        if (menuItemExit != null) {
+            menuItemExit.setVisible(false);
+        }
     }
 
     @Override
