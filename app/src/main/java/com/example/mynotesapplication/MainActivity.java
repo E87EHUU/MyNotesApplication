c582620440950160e0fcbc096338a63ba0acd4ea
E87EHUU <105707796+E87EHUU@users.noreply.github.com>
2022-11-21 23:18:23 +0300

    Урок 8. Внедрение меню. Обработка пункта "О программе."

--- app/src/main/java/com/example/mynotesapplication/MainActivity.java
+++ app/src/main/java/com/example/mynotesapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.mynotesapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +20,29 @@ public class MainActivity extends AppCompatActivity {
                 .add(R.id.notes_container, new NotesFragment())
                 .commit();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_about:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("")
+                        .add(R.id.notes_container, new AboutFragment())
+                        .commit();
+                break;
+            case R.id.action_exit:
+                finish();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
-//
