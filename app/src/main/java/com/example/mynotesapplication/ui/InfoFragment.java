04efc3a7ed481678451c88cd3f84006621807553
E87EHUU <e_zvyagin@mail.ru>
2023-12-05 20:38:52 +0300

    12-ое дз

---
+++ app/src/main/java/com/example/mynotesapplication/ui/InfoFragment.java
@@ -0,0 +1,11 @@
+package com.example.mynotesapplication.ui;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.mynotesapplication.R;
+
+public class InfoFragment extends Fragment {
+    public InfoFragment(){
+        super(R.layout.fragment_info);
+    }
+}
