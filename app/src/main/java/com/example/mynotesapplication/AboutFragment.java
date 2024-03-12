9a8ae421ee480da7660df8b2fab83c5ba3a752b2
E87EHUU <e_zvyagin@mail.ru>
2024-03-12 23:30:15 +0300

    test

--- app/src/main/java/com/example/mynotesapplication/AboutFragment.java
+++ app/src/main/java/com/example/mynotesapplication/AboutFragment.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 
 public class AboutFragment extends Fragment {
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
